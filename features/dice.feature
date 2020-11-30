46526055b85d726fc4842ab14810aa71cbb94959
Allister MacLeod <amacleod@disruptorbeam.com>
2020-11-29 23:40:59 -0500

    Scenario for differing from six

--- features/dice.feature
+++ features/dice.feature
@@ -1,6 +1,14 @@
 Feature: rolling dice
+  As a gamer
+  In order to have random outcomes
+  I want to roll virtual dice
 
   Scenario: rolling one six-sided die
-    Given our dice rolling interface
+    Given the dice roller is set up
     When we ask to roll one die
     Then the total number should be between 1 and 6
+
+  Scenario: rolling lower than six
+    Given the dice roller is set up
+    When we roll a bunch of times
+    Then eventually the number should be something other than 6
--- features/steps/dice.py
+++ features/steps/dice.py
@@ -1,7 +1,9 @@
 from behave import given, when, then
 
+MANY = 100
 
-@given('our dice rolling interface')
+
+@given('the dice roller is set up')
 def dice_setup(context):
   assert context.client
 
@@ -11,7 +13,26 @@ def request_roll(context):
   context.response = context.client.get('/dice')
 
 
+@when('we roll a bunch of times')
+def request_many_rolls(context):
+  context.responses = []
+  for _ in range(MANY):
+    context.execute_steps('when we ask to roll one die')
+    context.responses.append(context.response)
+
+
 @then('the total number should be between {low} and {high}')
 def check_roll(context, low, high):
   total = context.response.json['total']
   assert int(low) <= total <= int(high)
+
+
+@then('eventually the number should be something other than {number}')
+def check_many_rolls(context, number):
+  avoided_number = int(number)
+  same = True
+  for response in context.responses:
+    total = response.json['total']
+    if total != avoided_number:
+      same = False
+  assert not same
